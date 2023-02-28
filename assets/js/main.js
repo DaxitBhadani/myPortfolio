13d526ca2b882af7a8a0c468ef3e38c565b3b2f6
Daxit Bhadani <daxitbhadani.retrytech@gmail.com>
2023-02-28 23:39:09 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,210 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const pageContainer = document.querySelector(".mainScroll");
+
+/* SMOOTH SCROLL */
+const scroller = new LocomotiveScroll({
+  el: pageContainer,
+  smooth: true
+});
+
+scroller.on("scroll", ScrollTrigger.update);
+
+ScrollTrigger.scrollerProxy(pageContainer, {
+  scrollTop(value) {
+    return arguments.length
+      ? scroller.scrollTo(value, 0, 0)
+      : scroller.scroll.instance.scroll.y;
+  },
+  getBoundingClientRect() {
+    return {
+      left: 0,
+      top: 0,
+      width: window.innerWidth,
+      height: window.innerHeight
+    };
+  },
+  pinType: pageContainer.style.transform ? "transform" : "fixed"
+});
+
+////////////////////////////////////
+////////////////////////////////////
+window.addEventListener("load", function () {
+  let pinBoxes = document.querySelectorAll(".pin-wrap > *");
+  let pinWrap = document.querySelector(".pin-wrap");
+  let pinWrapWidth = pinWrap.offsetWidth;
+  let horizontalScrollLength = pinWrapWidth - window.innerWidth;
+
+  // Pinning and horizontal scrolling
+
+  gsap.to(".pin-wrap", {
+    scrollTrigger: {
+      scroller: pageContainer, //locomotive-scroll
+      scrub: true,
+      trigger: "#sectionPin",
+      pin: true,
+      // anticipatePin: 1,
+      start: "top top",
+      end: pinWrapWidth
+    },
+    x: -horizontalScrollLength,
+    ease: "none"
+  });
+
+  ScrollTrigger.addEventListener("refresh", () => scroller.update()); //locomotive-scroll
+
+  ScrollTrigger.refresh();
+});
+
+
+$('.navbar-toggler').on('click', function (e) {
+  var $this = $(this);
+  e.preventDefault();
+  if ($('body').hasClass('menu-open')) {
+      $this.removeClass('active');
+      $('.nav-menu .nav-menu-inner > ul > li').each(function (i) {
+          var that = $(this);
+          setTimeout(function () {
+              that.removeClass('is-show');
+          }, i * 100);
+      });
+      setTimeout(function () {
+          $('.nav-menu').removeClass('nav-menu-show');
+      }, 800);
+      setTimeout(function () {
+        $('body').removeClass('menu-open');
+    }, 900);
+     
+  }
+  else {
+  
+      $('.nav-menu').addClass('nav-menu-show');
+  
+      $this.addClass('active');
+        $('body').addClass('menu-open');
+      setTimeout(function () {
+          $('.nav-menu .nav-menu-inner > ul > li').each(function (i) {
+              var that = $(this);
+              setTimeout(function () {
+                  that.addClass('is-show');
+              }, i * 100);
+          });
+      }, 500);
+  }
+});
+// cursor
+
+// set the starting position of the cursor outside of the screen
+let clientX = 100;
+let clientY = 100;
+const innerCursor = document.querySelector(".cursor--small");
+
+const initCursor = () => {
+  // add listener to track the current mouse position
+  document.addEventListener("mousemove", e => {
+    clientX = e.clientX;
+    clientY = e.clientY;
+  });
+  
+  // transform the innerCursor to the current mouse position
+  // use requestAnimationFrame() for smooth performance
+  const render = () => {
+    innerCursor.style.transform = `translate(${clientX}px, ${clientY}px)`;
+    // if you are already using TweenMax in your project, you might as well
+    // use TweenMax.set() instead
+    // TweenMax.set(innerCursor, {
+    //   x: clientX,
+    //   y: clientY
+    // });
+    
+    requestAnimationFrame(render);
+  };
+  requestAnimationFrame(render);
+};
+
+initCursor();
+
+let lastX = 0;
+let lastY = 0;
+let isStuck = false;
+let showCursor = false;
+let group, stuckX, stuckY, fillOuterCursor;
+
+const initCanvas = () => {
+  const canvas = document.querySelector(".cursor--canvas");
+  const shapeBounds = {
+    width: 25,
+    height: 25
+  };
+  paper.setup(canvas);
+  const strokeColor = "#fff";
+  const strokeWidth = 10;
+  const segments = 4;
+  const radius = 5;
+  
+  // we'll need these later for the noisy circle
+  const noiseScale = 150; // speed
+  const noiseRange = 4; // range of distortion
+  let isNoisy = false; // state
+  
+  // the base shape for the noisy circle
+  const polygon = new paper.Path.RegularPolygon(
+    new paper.Point(0, 0),
+    segments,
+    radius
+  );
+  polygon.strokeColor = strokeColor;
+  polygon.strokeWidth = strokeWidth;
+  polygon.smooth();
+  group = new paper.Group([polygon]);
+  group.applyMatrix = false;
+  
+  const noiseObjects = polygon.segments.map(() => new SimplexNoise());
+  let bigCoordinates = [];
+  
+  // function for linear interpolation of values
+  const lerp = (a, b, n) => {
+    return (1 - n) * a + n * b;
+  };
+  
+  // function to map a value from one range to another range
+  const map = (value, in_min, in_max, out_min, out_max) => {
+    return (
+      ((value - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+    );
+  };
+  
+  // the draw loop of Paper.js 
+  // (60fps with requestAnimationFrame under the hood)
+  paper.view.onFrame = event => {
+    // using linear interpolation, the circle will move 0.2 (20%)
+    // of the distance between its current position and the mouse
+    // coordinates per Frame
+    lastX = lerp(lastX, clientX, 0.2);
+    lastY = lerp(lastY, clientY, 0.2);
+    group.position = new paper.Point(lastX, lastY);
+  }
+}
+
+initCanvas();
+
+
+Splitting();
+
+AOS.init();
+
+new WOW().init();
+
+
+$(window).resize(function () {
+  var width = $(window).width();
+  if (width > 992) {
+      var scene = document.getElementById('js-scene');
+      var parallax = new Parallax(scene);
+  }
+})
+.resize();
+
+
+
+
